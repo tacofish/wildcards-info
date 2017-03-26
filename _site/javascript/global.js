0d7467c2f3775f89deeb93dec9a60763964e953f
tacofish <whoisthetaco@gmail.com>
2017-03-26 14:21:17 -0700

    Fixup for github pages.

--- _site/javascript/global.js
+++
@@ -1,7 +0,0 @@
-function toggleNav() {
-    if (document.getElementById("mySidenav").style.width === "250px") {
-        return document.getElementById("mySidenav").style.width = "0";
-    } else {
-        return document.getElementById("mySidenav").style.width = "250px";
-    }
-}
